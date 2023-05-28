baf5a9ce0fb616fc8245a348cc5851c263fbf34b
Hashirama Senju <117456237+Rohanvegita24@users.noreply.github.com>
2023-05-28 19:39:50 +0530

    Add files via upload

---
+++ Log.js
@@ -0,0 +1,6 @@
+function impersonate(id) {
+  if (confirm('Are you sure?')) {
+      var url = 'Account/PresentationImpersonateTo/' + id;
+      window.location.href = url C:\Users\Rohan\Documents\Web\Pharmacy-Management\index.html ;
+  }
+}
